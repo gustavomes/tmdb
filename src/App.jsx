cb9d85c68b07500cd1d6738e9b7f792a84384e0f
gustavomes <gustavomes@yahoo.com.br>
2025-01-04 21:57:01 -0300

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,17 @@
-// Importa o componente Link, além de Routes e Route, do react-router-dom.
-// - Link: usado para criar links de navegação sem recarregar a página.
-// - Routes: um contêiner para as rotas da aplicação.
-// - Route: define cada rota individual e o componente que será renderizado.
-import { Link, Routes, Route } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Navbar from './components/Navbar'
+import Movie from './pages/Movie'
 
-// Importa os estilos globais da aplicação, definidos no arquivo App.css.
-import './App.css';
 
-// Importa a página Home, que é exibida na rota inicial ("/").
-import Home from './pages/Home';
-
-// Importa a página Search, usada para a funcionalidade de busca, associada à rota "/search".
-import Search from './pages/Search';
-
-// Importa o componente Navbar, que geralmente contém links para navegação no site.
-import Navbar from './components/Navbar';
-
-// Importa a página Movie, que exibe detalhes de um filme específico, associada a uma rota dinâmica "/movie/:id".
-import Movie from './pages/Movie';
-
-// Define o componente principal App, que serve como o ponto inicial da aplicação.
 function App() {
+  
   return (
-    // O contêiner principal da aplicação, com a classe "App" para aplicar estilos globais.
     <div className="App">
-<<<<<<< HEAD
-      {/* Um contêiner adicional para centralizar o conteúdo e aplicar layout. */}
-      <div className="container">
-        {/* Renderiza o componente Navbar, exibido em todas as páginas. */}
-        <Navbar />
-        {/* Define as rotas da aplicação usando o componente Routes. */}
-        <Routes>
-          {/* Define a rota inicial ("/"), que renderiza o componente Home. */}
-          <Route exact path="/" element={<Home />} />
-          {/* Define a rota dinâmica "/movie/:id", que renderiza o componente Movie. */}
-          <Route path="/movie/:id" element={<Movie />} /> {/* aqui teve erro */}
-          {/* Define a rota "/search", que renderiza o componente Search. */}
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </div>
-=======
           <div className="container">
-          <Navbar /> {/* coment */}
+          <Navbar />
           <Routes>
             <Route exact path="/" element= {<Home />} />
             <Route path="/movie/:id" element= {<Movie />} /> {/* aqui teve erro */}
@@ -49,9 +19,8 @@ function App() {
           </Routes>
           </div>
           
->>>>>>> Dev
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
-import Movies from './pages/Movie.jsx'
-import Search from './pages/Search.jsx'
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <Router >
-    <App />
-    </Router>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
